839f75425f80a470e7d4debacf848d8b6158da1e
choiseohyun55 <qkr7576@naver.com>
2025-10-02 16:34:17 +0900

    hotel update
    # Conflicts:
    #	hoteldb.json
    #	pom.xml
    #	src/main/java/com/travelers/travelweb/domain/hotel/domain/Hotel.java

--- src/main/java/com/travelers/travelweb/domain/hotel/domain/Hotel.java
+++ src/main/java/com/travelers/travelweb/domain/hotel/domain/Hotel.java
@@ -1,48 +1,22 @@
 package com.travelers.travelweb.domain.hotel.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Getter
+@Setter
+@NoArgsConstructor   // 기본 생성자
+@AllArgsConstructor  // 모든 필드 받는 생성자
+@Builder             // 빌더 패턴
 public class Hotel {
     private int id;
     private String name;
     private int city;
     private String country;
     private String image;
-    private String price;       // "₩123,456" 형태
+    private String price;
     private double rating;
-
-    // 생성자
-    public Hotel(int id, String name, int city, String country, String image, String price, double rating, int roomNum, String roomType) {
-        this.id = id;
-        this.name = name;
-        this.city = city;
-        this.country = country;
-        this.image = image;
-        this.price = price;
-        this.rating = rating;
-    }
-
-    // 기본 생성자
-    public Hotel() {}
-
-    // Getter / Setter
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public int getCity() { return city; }
-    public void setCity(int city) { this.city = city; }
-
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
-
-    public String getImage() { return image; }
-    public void setImage(String image) { this.image = image; }
-
-    public String getPrice() { return price; }
-    public void setPrice(String price) { this.price = price; }
-
-    public double getRating() { return rating; }
-    public void setRating(double rating) { this.rating = rating; }
-
 }
