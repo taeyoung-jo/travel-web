d7ce26178c0a15e553a0fc5bd8e88c0fbc4a4ccc
kkkwp <chxrryda@gmail.com>
2025-10-12 04:06:20 +0900

    fix: setter 추가

--- src/main/java/com/travelers/travelweb/domain/user/domain/User.java
+++ src/main/java/com/travelers/travelweb/domain/user/domain/User.java
@@ -5,9 +5,11 @@ import com.travelers.travelweb.global.config.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
+@Setter
 @SuperBuilder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
